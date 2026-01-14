ffa3a17c5f180c3f1a50caed6b8d562de17319d5
Eva <andersoner5@appstate.edu>
2026-01-14 14:22:55 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+# config.py
+# all of arties configs
+
+# IDS
+GUILD_ID = 1402420195221438554
+
+# Welcome IDS
+WELCOME_CHANNEL_ID = 1460698534696325292
+RULES_CHANNEL_ID = 1402420197834625045
+ROLES_CHANNEL_ID = 1460725594311495822
+INTRO_CHANNEL_ID = 1460715415440457778
+
+# Creative ops
+SELF_PROMO_CHANNEL_ID = 1460700818121687186
+JOBS_CHANNEL_ID = 1460498120654721237
+COMMISSIONS_CHANNEL_ID = 1402421153514913983
+
+# Lounge
+LOUNGE_CATEGORY_ID = 1460706656991182868
+
+
+
+# Roles
+RESONATOR_ROLE_NAME = "Resonator" 
+
+PRONOUN_ROLE_NAMES = ["She/Her", "She/They", "They/Them", 
+                      "He/They", "He/Him", "Any", "Ask"]
+
+MEDIUM_ROLE_NAMES = ["Digital", "Traditional", "Painting", "Ink", "Graphite",
+                    "3D", "Animation", "Writing", "Photography"]
---
+++ feats/welcome.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import config
+
+# welcome 
+class Welcome(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    async def send_welcome(self, member: discord.Member):
+        channel = member.guild.get_channel(config.WELCOME_CHANNEL_ID)
+        if not isinstance(channel, discord.TextChannel):
+            return
+
+        rules = member.guild.get_channel(config.RULES_CHANNEL_ID)
+        roles = member.guild.get_channel(config.ROLES_CHANNEL_ID)
+
+        rules_mention = rules.mention if rules else "#rules"
+        roles_mention = roles.mention if roles else "#roles"
+
+        await channel.send(
+            f"Hello {member.mention}, welcome to the Keyresonant Workshop!\n\n"
+            f"You’ll find the key to the rest of the server in {rules_mention}.\n"
+            f"When you’re ready, you can shape your presence in {roles_mention}.\n\n"
+        )
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        await self.send_welcome(member)
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Welcome(bot))
