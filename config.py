fe71ce85f2543259cfd66d5bda66f7bc2f471b68
Eva <andersoner5@appstate.edu>
2026-02-15 19:15:40 -0500

    Enhance config.py with comments and explanations
    Added comments for clarity and usage instructions.

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # config.py
 # all of arties configs
+# the reason we want all of arties configs here is so we can easily access them across the other files
 
 # IDS
 GUILD_ID = 1402420195221438554
@@ -21,10 +22,18 @@ LOUNGE_CATEGORY_ID = 1460706656991182868
 
 
 # Roles
+# to add a role command you'll want to name a new variable here and it will store the role data
+# you would do so by declaring it like so:
+# [VARIABLE_NAME] = "Roles actual name in discord"
+# variable names have no spaces and I prefer to keep them in all caps
+# for example this one only stores the resonator role in the RESONATOR_ROLE_NAME
 RESONATOR_ROLE_NAME = "Resonator" 
 
+# as you can see you can store multiple role names in an array of strings
+# an array of strings is just a techy way of saying this storing multiple items of type string
 PRONOUN_ROLE_NAMES = ["She/Her", "She/They", "They/Them", 
                       "He/They", "He/Him", "Any", "Ask"]
 
 MEDIUM_ROLE_NAMES = ["Digital", "Traditional", "Painting", "Ink", "Graphite",
                     "3D", "Animation", "Writing", "Photography"]
+
